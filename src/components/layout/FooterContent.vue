52524947f3205c10bea4b9c114edf70640daf1e0
poymanov <n.poymanov@gmail.com>
2022-08-16 22:05:41 +0300

    Добавлена базовая стилизация

---
+++ src/components/layout/FooterContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <ul class="navbar-list navbar-list-info">
+        <li class="navbar-item">
+          <a href="https://tocode.ru/courses/vuejs-3-s-nulya-do-rezultata/" class="navbar-link" target="_blank"
+            >Сделано на курсе "Vue.js 3 С нуля до результата"</a
+          >
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>

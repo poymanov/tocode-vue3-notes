75bec588e4413c8ce131234686743fba88520c95
poymanov <n.poymanov@gmail.com>
2022-08-16 22:22:26 +0300

    Добавлено меню навигации в шапку и подвал

--- src/components/layout/FooterContent.vue
+++ src/components/layout/FooterContent.vue
@@ -1,9 +1,16 @@
 <template>
   <footer class="footer">
     <div class="container">
+      <ul class="navbar-list">
+        <li class="navbar-item" v-for="item in menu" :key="item.alias">
+          <RouterLink :to="item.url" class="navbar-link">{{
+            item.title
+          }}</RouterLink>
+        </li>
+      </ul>
       <ul class="navbar-list navbar-list-info">
         <li class="navbar-item">
-          <a href="https://tocode.ru/courses/vuejs-3-s-nulya-do-rezultata/" class="navbar-link" target="_blank"
+          <a :href="courseUrl" class="navbar-link" target="_blank"
             >Сделано на курсе "Vue.js 3 С нуля до результата"</a
           >
         </li>
@@ -11,3 +18,16 @@
     </div>
   </footer>
 </template>
+
+<script>
+import { app } from "@/_config.js";
+
+export default {
+  data() {
+    return {
+      courseUrl: app.courseUrl,
+      menu: app.menu,
+    };
+  },
+};
+</script>
--- src/components/layout/HeaderContent.vue
+++ src/components/layout/HeaderContent.vue
@@ -3,9 +3,29 @@
     <div class="navbar">
       <div class="container">
         <div class="navbar-content">
-          <RouterLink to="/" class="navbar-logo">Notes</RouterLink>
+          <RouterLink to="/" class="navbar-logo">{{ title }}</RouterLink>
+          <ul class="navbar-list">
+            <li class="navbar-item" v-for="item in menu" :key="item.alias">
+              <RouterLink :to="item.url" class="navbar-link">{{
+                item.title
+              }}</RouterLink>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
   </header>
 </template>
+
+<script>
+import { app } from "@/_config.js";
+
+export default {
+  data() {
+    return {
+      title: app.title,
+      menu: app.menu,
+    };
+  },
+};
+</script>

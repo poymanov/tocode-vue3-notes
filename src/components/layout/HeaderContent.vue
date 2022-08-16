7604e027ac81b75461d933c0dd1564ae6cc74fb2
poymanov <n.poymanov@gmail.com>
2022-08-16 22:05:41 +0300

    Добавлена базовая стилизация

---
+++ src/components/layout/HeaderContent.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <div class="navbar">
+      <div class="container">
+        <div class="navbar-content">
+          <RouterLink to="/" class="navbar-logo">Notes</RouterLink>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ src/components/layout/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <HeaderContent />
+    <div class="wrapper-content">
+      <div class="section">
+        <div class="container">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+    <FooterContent />
+  </div>
+</template>
+
+<script>
+import HeaderContent from "@/components/layout/HeaderContent.vue";
+import FooterContent from "@/components/layout/FooterContent.vue";
+
+export default {
+  components: { HeaderContent, FooterContent },
+};
+</script>

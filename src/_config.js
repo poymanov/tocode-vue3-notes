ea0824b73e43de25fa2b36856ff3b0f41c45ef7b
poymanov <n.poymanov@gmail.com>
2022-08-16 22:19:59 +0300

    Добавлена информационная страница "О приложении"

---
+++ src/_config.js
@@ -0,0 +1,16 @@
+export const app = {
+  title: "Notes",
+  courseUrl: "https://tocode.ru/courses/vuejs-3-s-nulya-do-rezultata/",
+  menu: [
+    {
+      alias: "home",
+      url: "/",
+      title: "Home",
+    },
+    {
+      alias: "about",
+      url: "/about",
+      title: "About",
+    },
+  ],
+};
---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="view-sm isCenter">
+    <div class="md-body">
+      <h1>В этом приложении разработано:</h1>
+      <ul>
+        <li>Vue 3 + Vite</li>
+        <li>Полная настройка VUEX</li>
+        <li>Хранение заметок в localStorage</li>
+        <li>Передача данных между компонентами</li>
+        <li>Разделение логики по компонентам</li>
+        <li>Фильтрация Элементов</li>
+        <li>Создание глобальных конфигов</li>
+      </ul>
+      <p>
+        Сделано на курсе
+        <a :href="courseUrl" target="_blank" class="link"
+          >"Vue.js 3 С нуля до результата"</a
+        >
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { app } from "@/_config.js";
+
+export default {
+  data() {
+    return {
+      courseUrl: app.courseUrl,
+    };
+  },
+};
+</script>
